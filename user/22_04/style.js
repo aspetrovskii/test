2ef6bcc70a0bf77ff3e4e05c00d922a9c4e654af
aspetrovskii <aspetr2009@gmail.com>
2023-04-29 17:42:52 +0300

    newnew

---
+++ user/22_04/style.js
@@ -0,0 +1,39 @@
+// switch (expression){
+//     case n1:
+//         statements
+//         break;
+//     case n2:
+//         statements
+//         break;
+//     case n3:
+//         statements
+//         break;
+//     default:
+//         statements
+// }
+
+// do{
+//     code block
+// }
+// while (condition);
+
+// var sum =0;
+// for(i=4;i<8;i++){
+//     if (i==6){
+//         continue;
+//     }
+//     sum += i;
+// }
+// document.write(sum);
+
+// function myFunction() {
+//     alert('вы вызывали функцию')
+// }
+
+// myFunction()
+
+function myFunction(a,b) {
+    return a * b;
+}
+
+alert(myFunction(5,8), 'результат умножения')
